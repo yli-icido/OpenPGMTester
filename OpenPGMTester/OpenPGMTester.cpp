f16730bd8f9d364bd1883c650676fda4f8a5a996
Yang Liu <yli@icido.de>
2012-07-10 17:30:41 +0200

    change the way to launch sender or receiver

--- OpenPGMTester/OpenPGMTester.cpp
+++ OpenPGMTester/OpenPGMTester.cpp
@@ -11,54 +11,63 @@ using namespace std;
 int _tmain( int argc, _TCHAR* argv[] )
 {
     int retval = EXIT_FAILURE;
-    if ( argc < 2 ) // command and parameter indicates it is sender or receiver 
+    char userInputc[256];
+
+    do 
     {
-        _tprintf( _T("wrong parameters") );
-    }
-    else if ( _tcscmp( argv[1], _T("send") ) == 0 ) // send
-    {
-        do 
+        fprintf (stderr, "type \"send\" to send packages\n");
+        fprintf (stderr, "type \"receive\" to receive packages\n");
+        fprintf (stderr, "type \"q\" to quit\n");
+        gets( userInputc );
+
+        if ( strcmp( userInputc, "send" ) == 0 ) // send
         {
-            PGMSender* sender = new PGMSender();
-            if ( sender == NULL ) break;
+            do 
+            {
+                PGMSender* sender = new PGMSender();
+                if ( sender == NULL ) break;
 
-            if ( sender->init() != PGM_SUCCESS ) 
-                break;
+                if ( sender->init() != PGM_SUCCESS ) 
+                    break;
 
-            if ( sender->connect() != PGM_SUCCESS )
-                break;
+                if ( sender->connect() != PGM_SUCCESS )
+                    break;
 
-            if ( sender->send() != PGM_SUCCESS )
-                break;
+                if ( sender->send() != PGM_SUCCESS )
+                    break;
 
-            retval = EXIT_SUCCESS;
+                retval = EXIT_SUCCESS;
 
-        } while ( false );
-    }
-    else if ( _tcscmp( argv[1], _T("receive") ) == 0 ) // receive
-    {
-        do 
+            } while ( false );
+        }
+        else if ( strcmp( userInputc, "receive" ) == 0 ) // receive
         {
-            PGMReceiver* receiver = new PGMReceiver();
-            if ( receiver == NULL ) break;
+            do 
+            {
+                PGMReceiver* receiver = new PGMReceiver();
+                if ( receiver == NULL ) break;
 
-            if ( receiver->init() != PGM_SUCCESS )
-                break;
+                if ( receiver->init() != PGM_SUCCESS )
+                    break;
 
-            if ( receiver->connect() != PGM_SUCCESS )
-                break;
+                if ( receiver->connect() != PGM_SUCCESS )
+                    break;
 
-            //             if ( receiver->receive() != PGM_SUCCESS )
-            //                 break;
+                //             if ( receiver->receive() != PGM_SUCCESS )
+                //                 break;
 
+                retval = EXIT_SUCCESS;
+
+            } while ( false );
+        }
+        else if ( strcmp( userInputc, "q" ) == 0 )
+        {
+            fprintf(stderr, "quitting ...\n");
             retval = EXIT_SUCCESS;
+            break;
+        }
 
-        } while ( false );
-    }
-    else
-    {
-        _tprintf( _T("please provide parameter: send or receiver") );
-    }
+    } while ( strcmp( userInputc, "q" ) != 0 );
 
     return retval;
 }
